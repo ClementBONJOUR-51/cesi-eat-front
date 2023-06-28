24cf06ce9c2e1f9a8d4d2c9b6b4ec2f9522db345
Clément BONJOUR <bonjourclement51100@gmail.com>
2023-06-28 14:13:05 +0200

    adit image size

--- nuxt-app/components/components_customers/Card-customer.vue
+++ nuxt-app/components/components_customers/Card-customer.vue
@@ -1,33 +1,33 @@
 <template>
-    <div class="bg-emerald-400 max-w-xs rounded-[15px] shadow-lg">
-      <router-link :to="'/restaurant/' + restaurant._id">
-        <div class="px-0 py-0 grid grid-cols-1 gap-2">
-          <div class="col-span-full">
-            <img class="rounded-[15px] w-full" :src="restaurant.Img" />
+  <div class="bg-emerald-400 max-w-xs rounded-[15px] shadow-lg">
+    <router-link :to="'/restaurant/' + restaurant._id">
+      <div class="px-0 py-0 grid grid-cols-1 gap-2">
+        <div class="col-span-full">
+          <img class="rounded-[15px] w-full" :src="restaurant.Img" style="width: 300px; height: 200px;" />
+        </div>
+        <div class="col-span-full font-bold text-l md:text-xl mb-2 px-4 flex justify-between">
+          <div class="flex items-center">
+            {{ restaurant.restorant_name }}
           </div>
-          <div class="col-span-full font-bold text-l md:text-xl mb-2 px-4 flex justify-between">
-            <div class="flex items-center">
-              {{ restaurant.restorant_name }}
-            </div>
-            <div>
-              <!-- {{ restaurant.Note }} ({{ restaurant.Nombre_de_note }}+) -->
-            </div>
+          <div>
+            <!-- {{ restaurant.Note }} ({{ restaurant.Nombre_de_note }}+) -->
           </div>
         </div>
-      </router-link>
-    </div>
-  </template>
+      </div>
+    </router-link>
+  </div>
+</template>
   
-  <script>
-  
-  export default {
-    name: 'Card',
-    props: {
-      restaurant: {
-        type: Object,
-        required: true,
-      },
+<script>
+
+export default {
+  name: 'Card',
+  props: {
+    restaurant: {
+      type: Object,
+      required: true,
     },
-  };
-  </script>
+  },
+};
+</script>
   
