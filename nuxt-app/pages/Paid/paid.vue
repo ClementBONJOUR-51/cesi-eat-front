199b6b5aed834106ddaa8563f4fbdb5c38034965
Justine Quatresols <justine.quatresols@viacesi.fr>
2023-06-28 15:44:09 +0200

    client fini

--- nuxt-app/pages/Paid/paid.vue
+++ nuxt-app/pages/Paid/paid.vue
@@ -1,21 +1,24 @@
 <template>
     <div v-bind:key="order" v-for="order in orders">
-      <components_customersCard-orders :order="order" :fetchData="fetchData" :removeOrder="removeOrder" />
+      <components_customersCard-orders-notpaid :order="order" :fetchData="fetchData" :removeOrder="removeOrder" />
     </div>
   </template>
   
   <script>
   import axios from 'axios';
   import { onMounted, ref } from 'vue';
+  import jwt_decode from "jwt-decode";
   
   export default {
-    name: 'Livreur',
+    name: 'Paid',
     setup() {
       const orders = ref([]);
   
       const fetchData = async () => {
         try {
-          const response = await axios.get(`${useRuntimeConfig().public.api_base_url}/getOrdersNotPaidByCustomerId`);
+        const token = localStorage.getItem('authToken');
+        const decoded_user_id = jwt_decode(token);
+          const response = await axios.get(`${useRuntimeConfig().public.api_base_url}/getOrdersNotPaidByCustomerId/${decoded_user_id.id}`);
           return response.data.result.orders;
         } catch (error) {
           console.error(error);
